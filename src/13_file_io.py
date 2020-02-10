c94dcfaefc1db30518ac9ed52e957bbc48f7a920
Liam Sutton <devliam90@gmail.com>
2020-02-10 15:16:56 +0000

    files

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('./foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+
+print(f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +22,14 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+text = ('Hello, my name is Liam\n I am 29 years old\n I am from the United Kingdom\n')
+
+with open('./bar.txt', 'w') as f2:
+    f2.write(str(text))
+
+print(f2.closed)
+with open('./bar.txt') as f2:
+    read_data = f2.read()
+    print(read_data)
+
+print(f2.closed)
